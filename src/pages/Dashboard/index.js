7ec3b2b43ccd853287515f4947d0e4a75d140460
Jander Alves <jander.gustavo@gmail.com>
2022-06-23 15:42:03 -0300

    criando header responsivo

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,15 +1,17 @@
 import { AuthContext } from '../../contexts/auth';
 import { useContext } from 'react';
+import Header from '../../components/Header';
 
 
 export default function Dashboard(){
 
-    const { logout,user } = useContext(AuthContext);
+    const { logout } = useContext(AuthContext);
 
     return(
         <div>
-            <h1>Dashboard</h1>
-            <h1>{user.nome}</h1>
+            <Header/>
+
+            <h1>Dashboard</h1>            
             <button onClick={logout}>Logout</button>
         </div>
     );
